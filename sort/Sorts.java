6da91ff0ff0ef751c25fc371bee8d1026b30a137
polyqubit <polyumbrial@gmail.com>
2021-09-25 01:20:02 -0700

    bubble

--- sort/Sorts.java
+++ sort/Sorts.java
@@ -1,7 +1,98 @@
 public final class Sorts {
-	private int x;
-	public Sorts() {
-		
+	private double x;
+	private Sorts(double inp) {
+		x = inp;
 	}
 	
+	//hand: 0 = lowest on left, 1 = highest on left
+	public static void bubbleI(int flag, int[] inp, int hand) {
+		
+		boolean swap = true;
+		int round = 0;
+		int reg;
+		if(hand==0 || hand!=1) {
+				while(swap){
+					swap = false;
+					round++;
+					for(int i=0;i<inp.length-round;i++){
+						if(inp[i]>inp[i=1]){
+							reg = inp[i];
+							inp[i] = inp[i+1];
+							inp[i+1]=reg;
+							swap = true;
+						}
+					}
+				}
+				
+		}
+		
+		else if(hand==1) {
+				while(swap){
+					swap = false;
+					round++;
+					for(int i=0;i<inp.length-round;i++){
+						if(inp[i]<inp[i=1]){
+							reg = inp[i];
+							inp[i] = inp[i+1];
+							inp[i+1]=reg;
+							swap = true;
+						}
+					}
+				}
+		}
+		//if(flag == 1) {
+			System.out.print("[ ");
+			for(int i=0;i<inp.length;i++) {
+				System.out.print(inp[i]);
+				if(i<inp.length-1){
+					System.out.print(", ");
+				}
+			}
+			System.out.println(" ]");
+		//}
+	}
+	
+	public static void bubbleD(int flag, double[] inp, int hand) {
+		boolean swap = true;
+		int round = 0;
+		double reg;
+		if(hand==0 || hand!=1) {
+				while(swap){
+					swap = false;
+					round++;
+					for(int i=0;i<inp.length-round;i++){
+						if(inp[i]>inp[i=1]){
+							reg = inp[i];
+							inp[i] = inp[i+1];
+							inp[i+1]=reg;
+							swap = true;
+						}
+					}
+				}
+		}
+		else if(hand==1) {
+				while(swap){
+					swap = false;
+					round++;
+					for(int i=0;i<inp.length-round;i++){
+						if(inp[i]<inp[i=1]){
+							reg = inp[i];
+							inp[i] = inp[i+1];
+							inp[i+1]=reg;
+							swap = true;
+						}
+					}
+				}
+		}
+		//if(flag == 1) {
+			System.out.print("[ ");
+			for(int i=0;i<inp.length;i++) {
+				System.out.print(inp[i]);
+				if(i<inp.length-1){
+					System.out.print(", ");
+				}
+			}
+			System.out.println(" ]");
+		//}
+	}
 }
--- sort/starter.java
+++ sort/starter.java
@@ -7,6 +7,22 @@ public class starter implements InputControl, InputKeyControl {
 		// please leave following line alone, necessary for keyboard/mouse input
 		KeyController kC = new KeyController(Canvas.getInstance(),new starter());
 		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
+		
+		int flag = 0b0111111111;
+		
+		int[] arr = new int[10];
+		nMath tnum = new nMath(0);
+		System.out.print("[ ");
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = (int) tnum.rand(flag,10);
+			Canvas.pause(1);
+			if(i<arr.length-1){
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" ]");
+		
+		Sorts.bubbleI(1,arr,0);
 	}
 
 	public void onMouseClick(double x, double y) {
